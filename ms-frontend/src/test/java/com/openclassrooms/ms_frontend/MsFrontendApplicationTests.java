f927ac0dd56612ec46c6a5a1b380984335ebafd3
Scarounine <paulinelesueur93@gmail.com>
2024-10-15 14:50:35 +0200

    Initialize MediLabo Solutions project with microservices

---
+++ ms-frontend/src/test/java/com/openclassrooms/ms_frontend/MsFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.ms_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
