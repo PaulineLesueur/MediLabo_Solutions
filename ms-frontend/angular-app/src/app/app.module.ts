feda0938f206e616fe19b2441f51d965a776deb5
Scarounine <paulinelesueur93@gmail.com>
2024-10-16 13:21:49 +0200

    refactor: header component deleted

--- ms-frontend/angular-app/src/app/app.module.ts
+++ ms-frontend/angular-app/src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component'; // Importez HeaderComponent
 
 @NgModule({
   declarations: [
     AppComponent // Déclarez uniquement AppComponent ici
   ],
   imports: [
-    BrowserModule,
-    HeaderComponent // Importez HeaderComponent ici
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent] // Composant racine
