c86d2cd8275271487a9b788c6e9c7df0c650215c
Scarounine <paulinelesueur93@gmail.com>
2024-10-21 11:14:31 +0200

    route: patient-data -> patients-data

--- ms-frontend/angular-app/src/app/app.routes.ts
+++ ms-frontend/angular-app/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { PatientDataComponent } from './patient-data/patient-data.component';
 
 export const routes: Routes = [
-    { path: 'patient-data', component: PatientDataComponent }
+    { path: 'patients-data', component: PatientDataComponent }
 ];
