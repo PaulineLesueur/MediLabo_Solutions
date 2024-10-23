09cb006a2a758e70db3e2315720ff043622c7f15
Scarounine <paulinelesueur93@gmail.com>
2024-10-23 13:14:10 +0200

    angular: downgrade angular from v17 to v16

--- ms-frontend/angular-app/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { PatientDataComponent } from './patient-data/patient-data.component';
-
-export const routes: Routes = [
-    { path: 'patients-data', component: PatientDataComponent }
-];
