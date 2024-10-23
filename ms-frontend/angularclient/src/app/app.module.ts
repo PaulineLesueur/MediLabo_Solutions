e38669065628ca2d0c163714a5efe84187e00947
Scarounine <paulinelesueur93@gmail.com>
2024-10-23 13:53:01 +0200

    dowgrade angular from v17 to v16

---
+++ ms-frontend/angularclient/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PatientsListComponent } from './patients-list/patients-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PatientsListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ms-frontend/src/main/java/com/openclassrooms/ms_frontend/MsFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.ms_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFrontendApplication.class, args);
+	}
+
+}
