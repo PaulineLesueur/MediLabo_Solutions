5783a8938b1653a085ae5c9e0aefa67d8b566c40
Scarounine <paulinelesueur93@gmail.com>
2024-11-04 11:59:14 +0100

    Bug: Bean httpsecurity not found

--- ms-api-gateway/src/main/java/com/openclassrooms/ms_api_gateway/config/SecurityConfig.java
+++ ms-api-gateway/src/main/java/com/openclassrooms/ms_api_gateway/config/SecurityConfig.java
@@ -5,13 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
