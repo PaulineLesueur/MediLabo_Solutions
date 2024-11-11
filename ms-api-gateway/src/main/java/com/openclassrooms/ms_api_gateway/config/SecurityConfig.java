4ff72573ccbcd58a6953887b373c10f21e1f3bec
Scarounine <paulinelesueur93@gmail.com>
2024-11-11 16:23:36 +0100

    Refactor: fix gateway problems and delete user db and stuff

--- ms-api-gateway/src/main/java/com/openclassrooms/ms_api_gateway/config/SecurityConfig.java
+++ ms-api-gateway/src/main/java/com/openclassrooms/ms_api_gateway/config/SecurityConfig.java
@@ -2,25 +2,45 @@ package com.openclassrooms.ms_api_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers(HttpMethod.POST, "/login").permitAll()
-                            .anyRequest().authenticated();
-                })
-                .build();
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowCredentials(true);
+        corsConfig.applyPermitDefaultValues();
+        corsConfig.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:4200/*"));
+        corsConfig.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "DELETE", "PUT"));
+        corsConfig.setAllowedHeaders(List.of("*"));
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(source -> corsConfig))
+                .authorizeExchange(auth -> auth
+                        .pathMatchers("/public/**").permitAll()
+                        .pathMatchers("/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutUrl("/login?logout")
+                );
+
+        return http.build();
     }
 
     @Bean
