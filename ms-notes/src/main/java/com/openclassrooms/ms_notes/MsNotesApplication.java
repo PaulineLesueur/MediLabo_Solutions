1c55c8b848b4467bf7599fd9defb91b3abca970d
Scarounine <paulinelesueur93@gmail.com>
2024-11-15 14:41:28 +0100

    feat: initialize microservice notes

---
+++ ms-notes/src/main/java/com/openclassrooms/ms_notes/MsNotesApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.ms_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNotesApplication.class, args);
+	}
+
+}
---
+++ ms-notes/src/test/java/com/openclassrooms/ms_notes/MsNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.ms_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
