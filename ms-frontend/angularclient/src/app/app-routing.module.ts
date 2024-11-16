09d9bfbbfb6717b0a9e1dee731b4f1925589d3a1
Scarounine <paulinelesueur93@gmail.com>
2024-11-16 13:43:05 +0100

    feat: add authentification logic frontend

--- ms-frontend/angularclient/src/app/app-routing.module.ts
+++ ms-frontend/angularclient/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { PatientsListComponent } from './patients-list/patients-list.component';
 import { PatientDetailComponent } from './patient-detail/patient-detail.component';
 import { PatientFormComponent } from './patient-form/patient-form.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  //{ path: 'login', component: LoginFormComponent },
-  { path: 'patients-list', component: PatientsListComponent },
-  { path: 'patient/:id', component: PatientDetailComponent},
-  { path: 'patient/:id/update', component: PatientFormComponent},
-  { path: 'patients-list/create-patient', component: PatientFormComponent },
+  { path: 'login', component: LoginFormComponent },
+  { path: 'patients-list', component: PatientsListComponent, canActivate: [AuthGuard] },
+  { path: 'patient/:id', component: PatientDetailComponent, canActivate: [AuthGuard] },
+  { path: 'patient/:id/update', component: PatientFormComponent, canActivate: [AuthGuard] },
+  { path: 'patients-list/create-patient', component: PatientFormComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: 'patients-list', pathMatch: 'full' },
   { path: '**', redirectTo: 'patients-list' }
 ];
---
+++ ms-frontend/angularclient/src/app/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private token!: string;
+
+  login() {
+    this.token = 'token';
+  }
+
+  getToken(): string {
+    return this.token;
+  }
+
+  constructor() { }
+}
