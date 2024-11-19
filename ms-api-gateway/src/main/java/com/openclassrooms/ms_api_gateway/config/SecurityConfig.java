6fd80e14e79f9d22fac013e2158e9ac6fbbc9bc0
Scarounine <paulinelesueur93@gmail.com>
2024-11-19 12:00:21 +0100

    frontend: auth files

--- ms-api-gateway/src/main/java/com/openclassrooms/ms_api_gateway/config/SecurityConfig.java
+++ ms-api-gateway/src/main/java/com/openclassrooms/ms_api_gateway/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.openclassrooms.ms_api_gateway.config;
 import com.openclassrooms.ms_api_gateway.auth.JwtAuthenticationFilter;
 import com.openclassrooms.ms_api_gateway.auth.JwtAuthenticationProvider;
 import com.openclassrooms.ms_api_gateway.auth.JwtTokenUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -11,6 +13,9 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
 
 import java.util.List;
 
@@ -24,6 +29,21 @@ public class SecurityConfig {
         this.jwtTokenUtil = jwtTokenUtil;
     }
 
+    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+
+    @Bean
+    public WebFilter logFilter() {
+        return (ServerWebExchange exchange, WebFilterChain chain) -> {
+            String uri = exchange.getRequest().getURI().toString();
+            String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+
+            log.info("Request URI: {}", uri);
+            log.info("Authorization Header: {}", authHeader);
+
+            return chain.filter(exchange);
+        };
+    }
+
     @Bean
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http, JwtAuthenticationProvider jwtAuthenticationProvider) throws Exception {
 
@@ -33,17 +53,20 @@ public class SecurityConfig {
         corsConfig.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:4200/*"));
         corsConfig.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "DELETE", "PUT"));
         corsConfig.setAllowedHeaders(List.of("*"));
+        corsConfig.addExposedHeader("Authorization");
+
 
         http
                 .cors(cors -> cors.configurationSource(request -> corsConfig))
                 .csrf(csrf -> csrf.disable())
                 .authorizeExchange(auth -> auth
                         .pathMatchers("/public/**", "/auth/**").permitAll()
-                        .pathMatchers("/patients", "/patient/{id}").hasAnyRole("ROLE_ORGANIZER", "PRACTITIONER")
+                        .pathMatchers("/patients", "/patient/{id}").hasAuthority("ORGANIZER")
                         .pathMatchers("/patient/{id}/update", "/patients/create").hasRole("ORGANIZER")
                         .anyExchange().authenticated()
                 )
-                .addFilterBefore(new JwtAuthenticationFilter(jwtAuthenticationProvider), SecurityWebFiltersOrder.AUTHENTICATION);
+                .addFilterBefore(new JwtAuthenticationFilter(jwtAuthenticationProvider), SecurityWebFiltersOrder.AUTHENTICATION)
+        ;
 
         return http.build();
     }
