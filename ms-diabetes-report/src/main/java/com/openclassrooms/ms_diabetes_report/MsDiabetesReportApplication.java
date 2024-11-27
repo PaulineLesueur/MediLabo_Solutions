cc3202e66f0fac1039f1d2cdb9bc1b6297e6660b
Scarounine <paulinelesueur93@gmail.com>
2024-11-27 15:31:52 +0100

    feat: add diabetes-report microservice

---
+++ ms-diabetes-report/src/main/java/com/openclassrooms/ms_diabetes_report/MsDiabetesReportApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.ms_diabetes_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDiabetesReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDiabetesReportApplication.class, args);
+	}
+
+}
