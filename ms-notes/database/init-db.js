99d65605b1ee6127bb23ca2631089446d0261293
Scarounine <paulinelesueur93@gmail.com>
2024-12-10 17:25:15 +0100

    remove spring application for frontend + dockerise ui. ms-notes bug

--- ms-notes/database/init-db.js
+++ ms-notes/database/init-db.js
@@ -1,4 +1,10 @@
-db = db.getDB("MediLaboSolutions");
-const data = JSON.parse(cat('data.json'));
+print("Démarrage de l'initialisation de la base de données...");
 
+db = new Mongo("mongodb://root:rootroot@mongodb:27017/MediLaboSolutions");
+db.createCollection("notes");
+
+const data = JSON.parse(cat('data.json'));
+print("Insertion des données...");
 db.notes.insertMany(data);
+
+print("Insertion terminée.");
