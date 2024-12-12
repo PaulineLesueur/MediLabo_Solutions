a2204917feaab124262c4bce2b3305bc8b4fcc5f
Scarounine <paulinelesueur93@gmail.com>
2024-12-12 14:51:20 +0100

    refactor: remove spring microservice frontend

--- frontend/src/app/models/patient.ts
+++
@@ -1,9 +0,0 @@
-export class Patient {
-    id!: number;
-    lastName!: string;
-    firstName!: string;
-    birthdate!: Date;
-    gender!: string;
-    address!: string;
-    phone!: string;   
-}
