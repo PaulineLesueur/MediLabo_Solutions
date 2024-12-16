5e038b4f5cec4197fff501581005070c1f3cbc60
Scarounine <paulinelesueur93@gmail.com>
2024-12-16 16:45:26 +0100

    bug: diabetes-report does'nt work

--- ms-diabetes-report/src/main/java/com/openclassrooms/ms_diabetes_report/client/PatientClient.java
+++ ms-diabetes-report/src/main/java/com/openclassrooms/ms_diabetes_report/client/PatientClient.java
@@ -1,11 +1,12 @@
 package com.openclassrooms.ms_diabetes_report.client;
 
 import com.openclassrooms.ms_diabetes_report.DTO.PatientDTO;
+import com.openclassrooms.ms_diabetes_report.config.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-api-gateway", url = "http://ms-gateway:8080")
+@FeignClient(name = "ms-api-gateway", url = "http://ms-gateway:8080", configuration = FeignClientConfig.class)
 public interface PatientClient {
     @GetMapping("/patient/{id}")
     PatientDTO getPatientById(@PathVariable("id") int id);
